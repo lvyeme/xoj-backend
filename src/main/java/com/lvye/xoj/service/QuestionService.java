935b08697735c1d22fd187f6afb7c9644f1d6815
Sagasu <62923489+lvyeme@users.noreply.github.com>
2024-03-14 08:45:18 -0500

    完善questionService

--- src/main/java/com/lvye/xoj/service/QuestionService.java
+++ src/main/java/com/lvye/xoj/service/QuestionService.java
@@ -1,7 +1,14 @@
 package com.lvye.xoj.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.lvye.xoj.model.dto.question.QuestionQueryRequest;
+import com.lvye.xoj.model.entity.Question;
 import com.lvye.xoj.model.entity.Question;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.lvye.xoj.model.vo.QuestionVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
 * @author 老子
@@ -10,4 +17,37 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface QuestionService extends IService<Question> {
 
+    /**
+     * 校验
+     *
+     * @param question
+     * @param add
+     */
+    void validQuestion(Question question, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param questionQueryRequest
+     * @return
+     */
+    QueryWrapper<Question> getQueryWrapper(QuestionQueryRequest questionQueryRequest);
+
+    /**
+     * 获取题目封装
+     *
+     * @param question
+     * @param request
+     * @return
+     */
+    QuestionVO getQuestionVO(Question question, HttpServletRequest request);
+
+    /**
+     * 分页获取题目封装
+     *
+     * @param questionPage
+     * @param request
+     * @return
+     */
+    Page<QuestionVO> getQuestionVOPage(Page<Question> questionPage, HttpServletRequest request);
 }
